29374fdd6480956008e7e146be77234047f58b04
gavinstark81 <gavinstark81@gmail.com>
2022-06-17 14:25:20 -0500

    gavins 3rd try

--- script.js
+++ script.js
@@ -10,7 +10,22 @@ function makeRow(num) {
   table.appendChild(tr);
 }
 
-for (let i = 0; i < 3; i++) {
+// for (let i = 0; i < 3; i++) {
+//   makeRow(20);
+//   console.log("Running");
+// }
+
+let button = document.querySelector("button");
+button.addEventListener("click", function (e) {
   makeRow(20);
-  console.log("Running");
-}
+});
+
+table.addEventListener("click", function (e) {
+  console.log("clicked!");
+  let square = e.target;
+  if (square.className === undefined || square.className === "") {
+    square.className = "aquamarine";
+  } else {
+    square.className = "";
+  }
+});
