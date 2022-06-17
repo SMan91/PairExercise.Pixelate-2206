a12bdfb58a9e5ae79cd0f48259e84140c7c4b8e6
SMan91 <stephan.manley@yahoo.com>
2022-06-17 12:17:11 -0600

    v2 a few additional changes

--- script.js
+++ script.js
@@ -3,5 +3,14 @@ let table = document.querySelector("table");
 
 function makeRow(num) {
   let tr = document.createElement("tr");
-  for (let i = 0; i <= num; i++) {}
+  for (let i = 0; i < num; i++) {
+    let td = document.createElement("td");
+    tr.appendChild(td);
+  }
+  table.appendChild(tr);
+}
+
+for (let i = 0; i < 3; i++) {
+  makeRow(20);
+  console.log("Running");
 }
